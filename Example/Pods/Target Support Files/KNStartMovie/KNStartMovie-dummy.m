6051f3ddae0aaa284dbba19624585029db25f482
Leessang <992275294@qq.com>
2018-05-30 17:13:35 +0800

    第一次提交

---
+++ Example/Pods/Target Support Files/KNStartMovie/KNStartMovie-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KNStartMovie : NSObject
+@end
+@implementation PodsDummy_KNStartMovie
+@end
---
+++ Example/Pods/Target Support Files/Pods-KNStartMovie_Example/Pods-KNStartMovie_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KNStartMovie_Example : NSObject
+@end
+@implementation PodsDummy_Pods_KNStartMovie_Example
+@end
